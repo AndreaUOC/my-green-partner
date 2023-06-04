e01cc90b3b8320a4a858144068ba9f15e27b3ff0
Andrea <garrigatorrens@gmail.com>
2023-06-04 02:01:20 +0200

    db

---
+++ database/migrations/2023_05_06_172224_create_answers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->integer('question_id')->unsigned();
+            $table->text('content');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('answers', function (Blueprint $table) {
+            $table->dropForeign('answers_user_id_foreign');
+            $table->dropForeign('answers_question_id_foreign');
+        });
+
+        Schema::dropIfExists('answers');
+    }
+};
---
+++ database/migrations/2023_05_20_455789_create_plant_sunlight_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plants_sunlights', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('plant_id')->unsigned();
+            $table->integer('sunlight_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('plant_id')->references('id')->on('plants')->onDelete('cascade'); 
+            $table->foreign('sunlight_id')->references('id')->on('sunlights')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('plants_sunlights', function (Blueprint $table){
+            $table->dropForeign('plants_sunlights_plant_id_foreign');
+            $table->dropForeign('plants_sunlights_sunlights_id_foreign');
+        });
+        
+        Schema::dropIfExists('plants_sunlights');
+    }
+};
